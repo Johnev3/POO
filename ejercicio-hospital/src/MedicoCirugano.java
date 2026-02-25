7de2a92664f375303a4feff2465d79fb788aea5d
Johnev3 <johne.arredondo@autonoma.edu.co>
2026-02-25 14:40:51 -0500

    Creación clase MedicoCirugano

--- ejercicio-hospital/src/MedicoCirugano.java
+++ ejercicio-hospital/src/MedicoCirugano.java
@@ -1,10 +1,34 @@
+/**
+ * Clase MedicoCirugano
+ */
 public class MedicoCirugano extends Medico implements ICirugano{
+    /**
+     * numero de quirofano asignado
+     */
     int numeroQuirofano;
+
+    /**
+     *
+     * @param nombre nombre del cirgunano
+     * @param dni documento del cirgunano
+     * @param edad edad del cirugano
+     * @param genero genero del cirugano
+     * @param especialidad especilidad del cirugano
+     * @param numeroRegistro numero de registro del cirugano
+     * @param numeroQuirofano numero de quirofano asignado
+     */
     public MedicoCirugano(String nombre, String dni, int edad, String genero, String especialidad, String numeroRegistro,int numeroQuirofano) {
+        /**
+         * atributos heredados de la clase medico
+         */
         super(nombre, dni, edad, genero, especialidad, numeroRegistro);
         this.numeroQuirofano = numeroQuirofano;
     }
 
+    /**
+     * Metodo de interfaz validar quirofano
+     * @param estado estado del quirofano
+     */
     @Override
     public void validarQuirofano(boolean estado) {
         if(estado){
