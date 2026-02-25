bf41d697b70b66edd730aa4289b5ec221dc302ba
Johnev3 <johne.arredondo@autonoma.edu.co>
2026-02-25 14:46:21 -0500

    Creación clase Persona

--- ejercicio-hospital/src/Persona.java
+++ ejercicio-hospital/src/Persona.java
@@ -1,9 +1,28 @@
 public abstract class Persona {
+    /**
+     * nombre persona
+     */
     protected String nombre;
+    /**
+     * documento persona
+     */
     protected String dni;
+    /**
+     * edad persona
+     */
     protected int edad;
+    /**
+     * genero persona
+     */
     protected String genero;
 
+    /**
+     *
+     * @param nombre nombre persona
+     * @param dni documento persona
+     * @param edad edad persona
+     * @param genero genero persona
+     */
     public Persona(String nombre, String dni, int edad, String genero) {
         this.nombre = nombre;
         this.dni = dni;
@@ -11,5 +30,8 @@ public abstract class Persona {
         this.genero = genero;
     }
 
+    /**
+     * Metodo abstracto presentacion
+     */
     abstract void presentacion();
 }
