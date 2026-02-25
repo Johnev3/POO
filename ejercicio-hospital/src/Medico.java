a7708e5ad10dc20f53593449d7dbe323ebe99324
Johnev3 <johne.arredondo@autonoma.edu.co>
2026-02-25 14:37:52 -0500

    Creación clase Medico

--- ejercicio-hospital/src/Medico.java
+++ ejercicio-hospital/src/Medico.java
@@ -1,21 +1,54 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase Medico
+ */
 public class Medico extends Persona{
+    /**
+     * especialidad del medico
+     */
     private String especialidad;
+    /**
+     * nuemero de registro del medico
+     */
     private String numeroRegistro;
+    /**
+     * Lista de los pacientes asignados
+     */
     private List<Paciente> pacientesAsignados = new ArrayList<Paciente>();
 
+    /**
+     *
+     * @param nombre nombre medico
+     * @param dni dni medico
+     * @param edad edad medico
+     * @param genero genero medico
+     * @param especialidad especialidad medico
+     * @param numeroRegistro numero de registro medico
+     */
     public Medico(String nombre, String dni, int edad, String genero, String especialidad, String numeroRegistro) {
+        /**
+         * atributos heredados de la clase Persona
+         */
         super(nombre, dni, edad, genero);
         this.especialidad = especialidad;
         this.numeroRegistro = numeroRegistro;
     }
 
+    /**
+     * Metodo asignar Pacientes
+     * agrega el paciente a la lista del medico
+     * @param paciente objeto de tipo Paciente
+     */
     public void asignarPaciente(Paciente paciente){
         pacientesAsignados.add(paciente);
     }
 
+    /**
+     * Metodo revisar Pacientes
+     * Muestra la lista de los pacientes a revisar
+     */
     public void revisarPacientes(){
         System.out.println("\nDr. "+nombre+" revisando la lista de pacientes: ");
         for(Paciente paciente:pacientesAsignados){
@@ -23,6 +56,10 @@ public class Medico extends Persona{
         }
     }
 
+    /**
+     * Metodo asignarPrioridad
+     * Recorre la lista de pacientes y determina el nivel de prioridad
+     */
     public void asignarPrioridad(){
         for(Paciente paciente:pacientesAsignados){
             String sintoma = paciente.mostrarSintoma();
@@ -42,6 +79,9 @@ public class Medico extends Persona{
         }
     }
 
+    /**
+     * metodo abstracto presentacion
+     */
     @Override
     void presentacion() {
         System.out.println("[Medico] Dr. "+nombre+" - Especialidad: "+especialidad);
